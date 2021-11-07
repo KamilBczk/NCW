0c5b9f5efd5f520a3f2553e6e6de2735fd0325e1
Kamil Biczyk <kamil.biczyk.pro@gmail.com>
2021-11-07 14:07:46 +0100

    demo

--- demo/assets/js/header.js
+++ demo/assets/js/header.js
@@ -7,13 +7,19 @@
 
     // Function who set opacity to 0 of all elements & set opacity to element to show
     let linkJsClicked = function () {
+        for (let i = 0; i < linksJs.length; i++) {
+            linksJs[i].classList.remove("active");
+        }
+        this.classList.add("active");
         let contentToShow = this.getAttribute("data-content-toshow")
         let contentItems = document.getElementsByClassName("content-item");
-
+    
         for (let i = 0; i < contentItems.length; i++) {
             contentItems[i].style.opacity = 0;
+            contentItems[i].style.display = "none";
         }
         document.getElementById(contentToShow).style.opacity = 1;
+        document.getElementById(contentToShow).style.display = "block";
     }
 
     // Add eventlistener to all a elements with with class 'link-js'
