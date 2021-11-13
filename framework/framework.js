ed2d50176b9dd50bc3088bdee137e505b8c179e1
Kamil Biczyk <kamil.biczyk.pro@gmail.com>
2021-11-13 16:29:32 +0100

    framework

---
+++ framework/framework.js
@@ -0,0 +1,13 @@
+const addColor = (color, name) => {
+    const s = new Option().style;
+    s.color = color;
+    if (s.color === "") {
+        console.log("Undefined color");
+    }
+    else
+    {
+        if () {
+
+        }
+    }
+}
